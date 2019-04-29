28f478c8eb3093d304d3d8df36daf95323b31a11
Samik Maniar <samik.maniar@gatewaygroup.com>
2019-04-29 16:08:36 +0530

    made changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,33 +10,64 @@ class About extends Component {
                 <div className="inner-container">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col content-part">
-                                <div className="about-title">
-                                    <p>Hello, I’m a Patrick, web-developer based on Paris.<br></br>
+                            <div className="col-sm-6 about-banner"></div>
+                            <div className="col-sm-6 content-part ">
+                                <div className="about-text">
+                                    <p><span>Hello, I’m a Patrick,</span> web-developer based on Paris.<br></br>
                                         I have rich experience in web site design & building<br></br>
                                         and customization. Also I am good at html, css,<br></br>
                                         wordpress, php, jquery, bootstrap.
-                                </p>
-                                    <a href="#" className="btn-one"><i class="icon la la-download"></i>Download CV</a>
+                                     </p>
+                                    <a href="#" className="btn-one">Download CV</a>
                                 </div>
-
                                 <div className="row">
-                                    <div className="col">
-                                        <div class="detail-title"><span>My</span> Experience</div>
-                                        <div class="sec-resume resumee dfpad ">
-                                            <div class="resume-items card-box">
-                                                <div class="resume-item">
-                                                    <div class="name">Web Development</div>
-                                                    <div class="date">2013-Present<span> | </span>Facebook Inc.</div>
-                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>
+                                    <div className="col-sm-6">
+                                        <div className="card-items">
+                                            <div className="card-title"><span>My</span> Experience</div>
+                                            <div className="card-box">
+                                                <div className="card-item">                                                
+                                                    <div className="name">Web Development</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
+                                                </div>
+                                                <div className="card-item">                                                
+                                                    <div className="name">Front-end Developer</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
+                                                </div>
+                                                <div className="card-item">                                                
+                                                    <div className="name">Senior Developer</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="col"></div>
+                                        </div>
+                                        <div className="col-sm-6">
+                                        <div className="card-items">
+                                            <div className="card-title"><span>My</span> Education</div>
+                                            <div className="card-box">
+                                                <div className="card-item">                                                
+                                                    <div className="name">Art University</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
+                                                </div>
+                                                <div className="card-item">                                                
+                                                    <div className="name">Programming Course</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
+                                                </div>
+                                                <div className="card-item">                                                
+                                                    <div className="name">Web Design Course</div>
+                                                    <div className="date">2013-Present<span> | </span>Facebook Inc.</div>
+                                                    <p>Collaborate with creative and development teams on the execution of ideas.</p>                                                
+                                                </div>
+                                            </div>
+                                        </div>                                        
+                                    </div>                                    
                                 </div>
                             </div>
-                            <div className="col about-banner"></div>
+                            
                         </div>
                     </div>
                 </div>
